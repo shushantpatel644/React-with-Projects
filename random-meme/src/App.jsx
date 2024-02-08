62591223ca97821caf2123894fda3c05366a8eea
shushantpatel644 <patelshushant695@gmail.com>
2024-02-08 21:28:16 +0530

    8 feb

--- random-meme/src/App.jsx
+++ random-meme/src/App.jsx
@@ -1,9 +1,19 @@
+import React from "react";
+import Random from "./components/Random";
+import Tag from "./components/Tag";
 
 
 export default function App() {
   return (
-    <div>
-      Random Gifs
+    <div className="w-full h-screen flex flex-col background relative items-center overflow-x-hidden">
+      <h1 className="bg-warning rounded-lg uppercase w-11/12 text-center mt-[40px] ml-[15px] py-2 px-10 text-4xl font-bold ">
+        Random Gifs
+      </h1>
+      <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
+        <Random />
+        <Tag />
+      </div>
     </div>
   );
 }
+
