f43ebce7c0e2a01d97793322bdacbae7ae78e3b5
shushantpatel644 <patelshushant695@gmail.com>
2024-02-09 20:34:34 +0530

    React+ Vite

---
+++ React + Vite/01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+
+
+function App() {
+  return (
+    <h1>chai aur react wtih vite | SP </h1>
+  )
+} 
+export default App
---
+++ React + Vite/basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+   <h1>Chai aur code | Learn about vite and react how to setup and imp files and dire and github cmds</h1>
+  );
+}
+
+export default App;
