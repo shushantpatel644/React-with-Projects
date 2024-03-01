d7f4694cbd75f3dbde458e7f2657bff2ca43cc9e
shushantpatel644 <patelshushant695@gmail.com>
2024-03-01 14:43:12 +0530

    interview

---
+++ React + Vite/01vitereact/03tailwindprops/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+import Footer from './components/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Hey</h1>    
+    {/* <Footer/> */}
+    <Card/>
+    
+
+    </>
+
+  )
+}
+
+export default App
